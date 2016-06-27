08fc7dd2c25bb3c7cf706b2ba538e53fe80dcd64
Annie Hua <Annie@annies-air.aus.edu>
2016-06-27 18:34:53 +0400

    Done

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -1,8 +1,8 @@
 package com.flatironschool.javacs;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
@@ -11,41 +11,83 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+	final static Deque<String> parenthese  = new ArrayDeque<String>();
+	final static boolean isPhilosophy = false;
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
 	 * 
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
 	 * 
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
+	 * 2. Ignoring external links, links to the current page, or red links
+	 * 3. Stopping when reaching "Philosophy", a page with no links or a page
+	 *    that does not exist, or when a loop occurs
 	 * 
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
-
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+	public static String firstLink (String url) throws IOException{
 		Elements paragraphs = wf.fetchWikipedia(url);
-
 		Element firstPara = paragraphs.get(0);
-		
+
 		Iterable<Node> iter = new WikiNodeIterable(firstPara);
 		for (Node node: iter) {
 			if (node instanceof TextNode) {
-				System.out.print(node);
+				//System.out.print(node);
+				String word = node.toString();
+				//check if node is parentheses
+				if (word.contains("(")){
+					parenthese.push("(");
+				}else if (word.contains(")")){
+					parenthese.pop();
+				}
+			}if(node instanceof Element){
+				Element e = (Element) node;
+				if (isValid(e)){
+					return e.attr("abs:href");
+				}
 			}
-        }
+		}
+		return null;
+	}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+	public static boolean isValid(Element e){
+		if (!e.tagName().equals("a")) {
+			return false;
+		}
+		// in italics
+		Element current = e;
+		while(current.parent() !=null){
+			if (current.tagName().equals("i") || current.tagName().equals("em")){
+				return false;
+			}
+			current = current.parent();
+		}
+		// in parenthesis
+		if (!parenthese.isEmpty()){
+			return false;
+		}return true;
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		// some example code to get you started
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+		while (!url.equals("https://en.wikipedia.org/wiki/Philosophy")){
+			if (url==null || url.equals("")){
+				System.out.println("No valid links!");
+			}else {
+				System.out.println(url);
+				url = firstLink(url);
+			}
+		}
+		if(url.equals("https://en.wikipedia.org/wiki/Philosophy")){
+			System.out.println("Philosphy page found");
+		}else {
+			System.out.println("Philosphy page not found");
+		}
 	}
 }
